4efbf955bd9b0b6fcb409ecbc1b2f7ad6c5c2b61
m-delacre <mdelacre@protonmail.com>
2022-09-17 23:26:59 +0200

    modification css version mobile page about

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-//import { Link } from 'react-router-dom';
-//import data from '../../mockapi/data';
 import Banner from "../../components/Banner";
 import HomeProfiles from "../../components/HomeProfiles";
 import bannerImg from '../../assets/images/Baniere.png'
+import './home.css'
 
 function Home() {
   return (
     <React.Fragment>
-      <Banner
-        img = {bannerImg}
-        text = "Chez vous, partout et ailleurs"
-      />
+      <div className="bannerPosition">
+        <Banner
+          img = {bannerImg}
+          text = "Chez vous, partout et ailleurs"
+        />
+      </div>
+      
       <HomeProfiles/>
     </React.Fragment>
   );
